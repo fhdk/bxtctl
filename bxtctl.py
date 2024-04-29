04acd6e372674e332bce597b7410a1e12c44d101
Frede Hundewadt <noreply@wonky.codeberg.org>
2024-04-29 16:41:02 +0200

    wip

--- bxtctl.py
+++ bxtctl.py
@@ -24,6 +24,7 @@ import argparse
 import json
 import os
 import requests
+from requests import utils
 from pwinput import pwinput
 from cmd import Cmd
 from pathlib import Path
@@ -34,7 +35,7 @@ version = "0.1.0"
 user_agent = F"{app_name}/{version}"
 config_dir = f"{Path.home()}/.config/{app_name}"
 config_file = f"config.json"
-bxt_commands = ["commit", "compare", "configure", "login", "ls", "shell"]
+bxt_commands = ["commit", "compare", "login", "ls", "reconfigure", "shell"]
 endpoint = {
     "auth": "api/auth",
     "sections": "api/sections/get",
@@ -46,12 +47,26 @@ endpoint = {
 
 
 class Packages:
-    def __init__(self):
-        pass
+    """
+    A set of packages from a given repo and architecture
+    """
+    def __init__(self, branch: str, repo: str, packages: list):
+        self._branch = branch
+        self._repo = repo
+        self._packages = packages
 
 
 class BxtSettings:
+    """
+    Main Settings Class
+    """
     def __init__(self, folder: str, file_name: str):
+        """
+        Intialize configuration
+        Creates the configuration folder and initialize or load configuraiton
+        :param folder:
+        :param file_name:
+        """
         self._url: str = ""
         self._token: str = ""
         self._name: str = ""
@@ -64,27 +79,62 @@ class BxtSettings:
             self._load_config()
 
     def get_token(self) -> str:
+        """
+        Return Bxt token
+        :return:
+        """
         return self._token
 
     def set_token(self, value: str):
+        """
+        Set Bxt token
+        :param value:
+        :return:
+        """
         self._token = value
 
     def get_name(self) -> str:
+        """
+        Return Bxt username
+        :return:
+        """
         return self._name
 
     def set_name(self, value: str):
+        """
+        Set Bxt username
+        :param value:
+        :return:
+        """
         self._name = value
 
     def get_url(self) -> str:
+        """
+        Return Bxt endpoint url
+        :return:
+        """
         return self._url
 
     def set_url(self, value: str):
+        """
+        Set the Bxt endpoint url
+        :param value:
+        :return:
+        """
         self._url = value
 
     def __str__(self):
+        """
+        Return textual representation of the configuration  class
+        :return:
+        """
         return f"BxtConfig(Url: '{self._url}', Name: '{self._name}', Token: '{self._token}')"
 
     def _save_config(self):
+        """
+        Initialize configuration and write empty config
+        :return:
+        """
         config = {
             "token": self._token,
             "name": self._name,
@@ -94,6 +144,11 @@ class BxtSettings:
             json.dump(config, outfile)
 
     def _load_config(self):
+        """
+        Load BxtCtl configuration
+        The function assumes the file exist
+        :return:
+        """
         try:
             with open(self._file_name, "r") as infile:
                 data = json.load(infile)
@@ -112,7 +167,6 @@ class BxtSettings:
                 self._url = f"{data['url']}"
             except (KeyError,) as e:
                 self._url = ""
-
         except (FileNotFoundError,):
             self._token = ""
             self._name = ""
@@ -121,7 +175,11 @@ class BxtSettings:
             pass
 
     def configure(self) -> bool:
-        options = _get_config_options()
+        """
+        Create BctCtl configuration
+        :return:
+        """
+        options = _cli_input_config_options()
         passwd = pwinput(prompt="password: ", mask="")
         self._name = options["name"]
         self._url = options["url"]
@@ -132,6 +190,10 @@ class BxtSettings:
         return True
 
     def login(self) -> bool:
+        """
+        request login credentials to get a token
+        :return:
+        """
         username = input(f"bxt user ({self._name}): ").strip()
         if username == "":
             username = self._name
@@ -144,6 +206,10 @@ class BxtSettings:
 
 
 class Shell(Cmd):
+    """
+    TODO
+    Interactive shell
+    """
     prompt = f"({app_name}) > "
 
     @staticmethod
@@ -165,19 +231,40 @@ class Shell(Cmd):
 
 
 class BxtAcl:
+    """
+    Bxt Access Control List
+    """
     def __init__(self, sections: list):
+        """
+        Initialize ACL using a sections object
+        :param sections:
+        """
         self._sections = sections
 
     def get_sections(self):
+        """
+        Return sections avaialable to token
+        :return:
+        """
         return self._sections
 
     def get_architectures(self) -> set:
+        """
+        Return architectures available in the ACL
+        :return:
+        """
+        if not self._sections:
+            return set()
         architectures = []
         for section in self._sections:
             architectures.append(section["architecture"])
         return set(architectures)
 
     def get_branches(self):
+        """
+        Return branches available in the ACL
+        :return:
+        """
         if not self._sections:
             return set()
         branches = []
@@ -186,6 +273,10 @@ class BxtAcl:
         return set(branches)
 
     def get_repositories(self):
+        """
+        Return repositories available in the ACL
+        :return:
+        """
         if not self._sections:
             return set()
         repositories = []
@@ -194,9 +285,41 @@ class BxtAcl:
         return set(repositories)
 
 
+def _cli_input_config_options() -> dict:
+    """
+    get config options from user input
+    :return:
+    """
+    options = {"name": "", "url": ""}
+    while True:
+        options["url"] = input(f"bxt http: ").strip()
+        options["name"] = input(f"bxt user: ").strip()
+        if options["url"] != "" and options["name"] != "":
+            return options
+
+
+def _http_prepare_session(token: str = None) -> requests.session():
+    """
+    prepare the http session
+    :param token:
+    :return:
+    """
+    session = requests.session()
+    session.headers.update({"User-Agent": user_agent})
+    if token is not None:
+        session.cookies.update({"token": token})
+    return session
+
+
 def _http_get_acl(url: str, token: str) -> list:
+    """
+    get acl for token
+    :param url:
+    :param token:
+    :return:
+    """
     try:
-        session = _build_session(token)
+        session = _http_prepare_session(token)
         session = session.get(url)
         return json.loads(session.text)
 
@@ -206,18 +329,15 @@ def _http_get_acl(url: str, token: str) -> list:
     return []
 
 
-def _get_config_options() -> dict:
-    options = {"name": "", "url": ""}
-    while True:
-        options["url"] = input(f"bxt http: ").strip()
-        options["name"] = input(f"bxt user: ").strip()
-        if options["url"] and options["name"] != "":
-            return options
-
-
 def _http_verify_token(url: str, token: str) -> bool:
+    """
+    verify a token
+    :param url:
+    :param token:
+    :return:
+    """
     try:
-        session = _build_session(token)
+        session = _http_prepare_session(token)
         session = session.get(url)
         if session.status_code == 200:
             return True
@@ -229,9 +349,16 @@ def _http_verify_token(url: str, token: str) -> bool:
 
 
 def _http_get_token(url: str, name: str, passwd: str) -> str:
+    """
+    request a token using credentials
+    :param url:
+    :param name:
+    :param passwd:
+    :return:
+    """
     credentials = {"name": name, "password": passwd}
     try:
-        session = _build_session()
+        session = _http_prepare_session()
         session = session.post(url, json=credentials)
         print(session)
         if session.status_code == 200:
@@ -246,20 +373,25 @@ def _http_get_token(url: str, name: str, passwd: str) -> str:
     return ""
 
 
-def _build_session(token: str = None) -> requests.session():
-    session = requests.session()
-    session.headers.update({"User-Agent": user_agent})
-    if token is not None:
-        session.cookies.update({"token": token})
-    return session
+def _http_get_packages(url: str, branch: str, repositoriy: str, architectue: str, token: str) -> list:
+    session = _http_prepare_session(token)
+    session = session.get(url, params={"branch": branch, "repository": repositoriy, "architecture": architectue})
+    return json.loads(session.text)
 
 
 class BxtCtl:
+    """
+    Main application object
+    """
     def __init__(self):
         pass
 
     @staticmethod
     def run():
+        """
+        Run the BxtCtl application
+        :return:
+        """
         config = BxtSettings(config_dir, config_file)
         # if config is uninitialized force setup config
         if config.get_url() == "" or config.get_name() == "":
@@ -288,9 +420,9 @@ class BxtCtl:
                             type=str,
                             help="Command to execute",
                             choices=bxt_commands)
-        parser.add_argument("-b", "--branch", type=str, choices=acl.get_branches(), help="Target Branch")
         parser.add_argument("-r", "--repo", type=str, choices=acl.get_repositories(), help="Target Repository")
-        parser.add_argument("-a", "--archictecre", type=str, choices=acl.get_architectures(), help="Target Architecture")
+        parser.add_argument("-a", "--architecture", type=str, choices=acl.get_architectures(), help="Target Architecture")
+        parser.add_argument("-b", "--branch", nargs="+", type=str, choices=acl.get_branches(), help="Target Branch")
         parser.add_argument("-p", "--package", type=str, nargs="+", help="Target Package")
         args = parser.parse_args()
 
@@ -300,12 +432,16 @@ class BxtCtl:
                 exit(1)
 
         if args.command == "commit":
-
             print("TODO - commit package to branch/repo/arch")
             exit(0)
 
-        if args.commmand == "compare":
+        if args.command == "compare":
             print("TODO - compare two packges in /repo/arch")
+            for b in args.branch:
+                packages = _http_get_packages(f"{config.get_url()}/{endpoint['packages']}", b, args.repo, args.architecture, config.get_token())
+
+                print(f"Branch: {b}")
+                print(f"Packages: {packages}")
             exit(0)
 
         if args.command == "ls":
@@ -322,9 +458,16 @@ class BxtCtl:
 
 
 def start():
+    """
+    Entry point for poetry
+    :return:
+    """
     app = BxtCtl()
     app.run()
 
 
 if __name__ == '__main__':
+    """
+    Main entry point
+    """
     start()
