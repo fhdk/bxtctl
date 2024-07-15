e88740c2a59e8d718ec781be88108dc8aabcaa5c
Frede Hundewadt <fhdk.6pmrw@8shield.net>
2024-07-15 13:06:40 +0200

    Add new command functions and update existing ones for better functionality
    A set of new command functions including 'do_ls', 'do_copy', 'do_move', and 'do_remove' were added to the program, enabling improved handling of workspace, copy, move, and remove operations. Existing command functions like 'do_login', 'do_configure', 'do_commit' were updated to provide better user feedback and error handling. Initial checks for unsupported operating systems were also added to prevent execution on unsupported platforms.

--- bxtctl.py
+++ bxtctl.py
@@ -23,6 +23,7 @@
 import argparse
 import cmd2
 from cmd2 import Cmd2ArgumentParser, with_argparser
+import subprocess
 import sys
 import os
 from Bxt.BxtAcl import BxtAcl
@@ -30,11 +31,69 @@ from Bxt.BxtConfig import BxtConfig
 from Bxt.BxtSession import BxtSession
 
 
+def do_ls(args):
+    """
+    List content of workspace
+    :param args:
+    :return:
+    """
+    subprocess.run(["bxt", "ls", args.workspace])
+
+
 class BxtCtl(cmd2.Cmd):
     """
-    Main application object
+    Main bctctl shell instance
     """
+    def __init__(self):
+        # shortcuts = dict(cmd2.DEFAULT_SHORTCUTS)
+        shortcuts = dict()
+        shortcuts.update({
+            "?": "help",
+            "!": "shell",
+            "cp": "bxtcopy",
+            "exit": "quit",
+            "lsp": "ls_path",
+            "lsr": "ls_repo",
+            "lsw": "ls_workspace",
+            "mv": "move",
+            "rm": "remove",
+            "up": "commit",
+            "upload": "commit",
+            "ws": "workspace"
+        })
+        super().__init__(shortcuts=shortcuts)
+        # To remove built-in commands entirely, delete
+        # the "do_*" function from the cmd2.Cmd class
+        del cmd2.Cmd.do_alias
+        del cmd2.Cmd.do_edit
+        del cmd2.Cmd.do_macro
+        del cmd2.Cmd.do_run_pyscript
+        del cmd2.Cmd.do_run_script
+        # if config is uninitialized force setup config
+        if self.config.get_url() == "" or self.config.get_name() == "":
+            self.poutput("Enter initial configuration")
+            if not self.config.configure():
+                self.perror("Initialization failed")
+                exit(1)
 
+        # if token is empty - login
+        if self.config.get_access_token() == "":
+            self.poutput("First time login")
+            if not self.config.login():
+                self.perror("Login failed")
+                exit(1)
+
+        self.prompt = f"({self.config.get_name()}@{self.config.get_hostname()}) $ "
+
+        self.pwarning(f"TODO - remove this block")
+        self.pwarning(f"---------- PERMISSIONS ---------- ")
+        self.pwarning(f"bxt user: '{self.config.get_name()}' has access to:")
+        self.pwarning(f"Branches      : {self.acl.get_branches()}")
+        self.pwarning(f"Repositories  : {self.acl.get_repositories()}")
+        self.pwarning(f"Architectures : {self.acl.get_architectures()}")
+        self.pwarning(f"--------------------------------- ")
+
+    prompt = f"nn@bxt $ "
     config = BxtConfig()
     if not config.valid_config():
         config.configure()
@@ -55,204 +114,264 @@ class BxtCtl(cmd2.Cmd):
     )
 
     acl = BxtAcl(sections)
-    # set workspace command
-    workspace = Cmd2ArgumentParser(description="Get or set workspace")
-    workspace.add_argument("-w", "--workspace", type=str, help="Full path to workspace")
     # ###############################################################
-    # list command
-    list_args = Cmd2ArgumentParser(
-        description="List content of repo branch architecture"
+    # list workspace content
+    list_workspace_args = Cmd2ArgumentParser(description="List workspace content")
+    list_workspace_args.add_argument(
+        "-l", "--long", action="store_true", help="use long list"
     )
-    list_args.add_argument(
-        "branch", type=str, help="Target Branch", choices=acl.get_branches()
+
+    # ###############################################################
+    # list folder content
+    list_folder_args = Cmd2ArgumentParser(description="List path content")
+    list_folder_args.add_argument(
+        "-l", "--long", action="store_true", help="use long list"
     )
-    list_args.add_argument(
-        "repo", type=str, help="Target Repository", choices=acl.get_repositories()
+    list_folder_args.add_argument(
+        "path", type=str, nargs="?", default=".", help="Path to list content"
     )
-    list_args.add_argument(
-        "arch", type=str, help="Target Artitecture", choices=acl.get_architectures()
+
+    # ###############################################################
+    # set workspace command
+    workspace_args = Cmd2ArgumentParser(description="Get or set workspace")
+    workspace_args.add_argument(
+        "-w", "--workspace", type=str, help="Full path to workspace"
+    )
+
+    # ###############################################################
+    # list repo command
+    list_repo_args = Cmd2ArgumentParser(
+        description="List content of remote bxt repository"
+    )
+    list_repo_args.add_argument(
+        "location",
+        type=str,
+        help=f"{acl.get_branches()}/{acl.get_repositories()}/{acl.get_architectures()}",
     )
     # ###############################################################
     # compare command
-    comp_args = Cmd2ArgumentParser(
+    compare_args = Cmd2ArgumentParser(
         description="Compare repo package across branches and architectures"
     )
-    comp_args.add_argument(
-        "-b",
-        "--branch",
+    compare_args.add_argument(
+        "-l",
+        "--location",
         type=str,
         nargs="*",
-        help="Branches to compare",
-        choices=acl.get_branches(),
+        help=f"{acl.get_branches()}/{acl.get_repositories()}/{acl.get_architectures()}",
     )
-    comp_args.add_argument(
-        "-r",
-        "--repo",
-        type=str,
-        nargs="*",
-        help="Repositories to compare",
-        choices=acl.get_repositories(),
-    )
-    comp_args.add_argument(
-        "-a",
-        "--arch",
-        type=str,
-        nargs="*",
-        help="Architecures to compare",
-        choices=acl.get_architectures(),
-    )
-    comp_args.add_argument(
+    compare_args.add_argument(
         "-p",
         "--package",
         type=str,
         nargs="?",
         help="Package(s) to compare (multiple -p can be passed)",
     )
+
     # ###############################################################
     # commit command
     commit_args = Cmd2ArgumentParser(description="Commit package(s) to repository")
     commit_args.add_argument(
-        "branch", type=str, nargs=1, help="Target Branch", choices=acl.get_branches()
+        "location",
+        type=str,
+        help=f"{acl.get_branches()}/{acl.get_repositories()}/{acl.get_architectures()}",
     )
     commit_args.add_argument(
-        "repo",
+        "-p",
+        "--package",
         type=str,
-        nargs=1,
-        help="Target Repository",
-        choices=acl.get_repositories(),
-    )
-    commit_args.add_argument(
-        "arch",
-        type=str,
-        nargs=1,
-        help="Target Architecture",
-        choices=acl.get_architectures(),
-    )
-    commit_args.add_argument(
-        "package",
-        type=str,
-        nargs="?",
-        help="package filename(s) (multiple files can be passed)",
+        nargs="+",
+        help="package name(s) (multiple files can be passed)",
     )
 
-    def __init__(self):
-        super().__init__()
-        # if config is uninitialized force setup config
-        if self.config.get_url() == "" or self.config.get_name() == "":
-            print("Enter initial configuration")
-            if not self.config.configure():
-                print("Initialization failed")
-                exit(1)
+    # ###############################################################
+    # copy command
+    bxt_copy_args = Cmd2ArgumentParser(description="Copy package(s) inside bxt storage")
+    bxt_copy_args.add_argument(
+        "-p",
+        "--package",
+        type=str,
+        nargs="+",
+        help=f"'pkgname {acl.get_branches()}/{acl.get_repositories()}/{acl.get_architectures()} {acl.get_branches()}/{acl.get_repositories()}/{acl.get_architectures()}'",
+    )
 
-        # if token is empty - login
-        if self.config.get_access_token() == "":
-            print("First time login")
-            if not self.config.login():
-                print("Login failed")
-                exit(1)
+    # ###############################################################
+    # move command
+    bxt_move_args = Cmd2ArgumentParser(description="Move package(s) inside bxt storage")
+    bxt_move_args.add_argument(
+        "-p",
+        "--package",
+        type=str,
+        nargs="+",
+        help=f"Package move from repo to repo: 'pkgname {acl.get_branches()}/{acl.get_repositories()}/{acl.get_architectures()} {acl.get_branches()}/{acl.get_repositories()}/{acl.get_architectures()}'",
+    )
 
-        self.prompt = f"({self.config.get_name()}@{self.config.get_hostname()}) $ "
+    # ###############################################################
+    # remove command
+    bxt_delete_args = Cmd2ArgumentParser(
+        description="Delete package(s) inside bxt storage"
+    )
+    bxt_delete_args.add_argument(
+        "-p",
+        "--package",
+        type=str,
+        nargs="+",
+        help=f"Package remove from repo: 'pkgname {acl.get_branches()}/{acl.get_repositories()}/{acl.get_architectures()}'",
+    )
 
-        print(f"TODO - remove this block")
-        print(f"---------- PERMISSIONS ---------- ")
-        print(f"bxt user: '{self.config.get_name()}' has access to:")
-        print(f"Branches      : {self.acl.get_branches()}")
-        print(f"Repositories  : {self.acl.get_repositories()}")
-        print(f"Architectures : {self.acl.get_architectures()}")
-        print(f"--------------------------------- ")
+    @with_argparser(bxt_delete_args)
+    def do_remove(self, args):
+        """
+        Remove one or more package(s) from bxt storage
+        :param args: the package(s) to remove from the target repo
+        """
+        self.poutput("TODO: implement removal")
 
-    @with_argparser(workspace)
+    @with_argparser(bxt_copy_args)
+    def do_copy(self, args):
+        """
+        Copy one or more package(s) within bxt storage
+        :param args: the package(s) to copy from source to target
+        """
+        self.poutput("TODO: implement copy")
+
+    @with_argparser(bxt_move_args)
+    def do_move(self, args):
+        """
+        Move one or more package(s) in bxt storage
+        :param args: the package(s) to move from source to target
+        """
+        self.poutput("TODO: implement move")
+
+    @with_argparser(workspace_args)
     def do_workspace(self, args):
         """
         Get or set workspace
-        :param args:
-        :return:
+        :param args: optional path to new workspace
         """
         if not args.workspace:
-            print(f"Current workspace is: {self.config.workspace}")
+            self.poutput(f"Current workspace is: {self.config.workspace}")
         else:
             self.config.workspace = args.workspace
             self.config.save()
 
-    @with_argparser(list_args)
-    def do_list(self, args):
+    @with_argparser(list_workspace_args)
+    def do_ls_workspace(self, args):
+        """
+        List content of current workspace
+        """
+        self.poutput("Current workspace is: " + self.config.workspace)
+        cmd = ["ls"]
+        if args.long:
+            cmd.insert(len(cmd) + 1, "-l")
+        cmd.insert(len(cmd) + 1, self.config.workspace)
+        subprocess.run(cmd)
+
+    @with_argparser(list_folder_args)
+    def do_ls_path(self, args):
+        """
+        List content of specified folder
+        :param args: full path to folder
+        """
+        cmd = ["ls"]
+        if args.long:
+            cmd.insert(len(cmd) + 1, "-l")
+        cmd.insert(len(cmd) + 1, args.path)
+        subprocess.run(cmd)
+
+    @with_argparser(list_repo_args)
+    def do_ls_repo(self, args):
         """
         List content of selected repo
-        :param args:
-        :return:
+        :param args: path to repo e.g. branch/repo/arch
         """
+        location = args.location.split("/")
         pkgs = self.bxt_session.get_packages(
             f"{self.config.get_url()}/{self.config.endpoint['pkgList']}",
-            args.branch,
-            args.repo,
-            args.arch,
+            location[0],
+            location[1],
+            location[2],
             self.config.get_access_token(),
         )
         for pkg in pkgs:
-            print(
+            self.poutput(
                 f"{pkg['name']:<30}: {pkg['poolEntries'][pkg['preferredLocation']]['version']}"
             )
-            print(pkg)
 
-    @with_argparser(comp_args)
+    @with_argparser(compare_args)
     def do_compare(self, args):
         """
         Compare branches
         :param args:
         :return:
         """
-        branches = args.branch
-        architectures = args.arch
-        from pprint import pprint
-
-        for branch in branches:
-            for arch in architectures:
-                archpkgs = self.bxt_session.get_packages(
-                    f"{self.config.get_url()}/{self.config.endpoint['pkgCompare']}",
-                    args.branch,
-                    args.repo,
-                    arch,
-                    self.config.get_access_token(),
-                )
-                if args.package is not None:
-                    pkgs = [x for x in archpkgs if x["name"] in args.package]
-                    for pkg in pkgs:
-                        pprint(pkg)
-                else:
-                    for pkg in archpkgs:
-                        pprint(pkg)
+        locations = args.location
+        print(locations)
+        # branches = args.branch
+        # architectures = args.arch
+        #
+        # for branch in branches:
+        #     for arch in architectures:
+        #         archpkgs = self.bxt_session.get_packages(
+        #             f"{self.config.get_url()}/{self.config.endpoint['pkgCompare']}",
+        #             args.branch,
+        #             args.repo,
+        #             arch,
+        #             self.config.get_access_token(),
+        #         )
+        #         if args.package is not None:
+        #             pkgs = [x for x in archpkgs if x["name"] in args.package]
+        #             for pkg in pkgs:
+        #                 self.poutput(pkg)
+        #         else:
+        #             for pkg in archpkgs:
+        #                 self.poutput(pkg)
 
     @with_argparser(commit_args)
     def do_commit(self, args):
         """
-        Commpit package to repo
+        Commpit package(s) to repo
         :param args:
         :return: True/False
         """
-        print(f"TODO - commit package to repo - using {args}")
-        print(f"Reading files from workspace: {self.config.workspace}")
-        print(f"Commit endpoint is: {self.config.endpoint['commitEndpoint']}")
-        print(f"Commit package(s) to: {args.branch}/{args.repo}/{args.arch}")
-        for pkg in args.package:
-            print(f"commit package: {pkg}")
+        location = args.location.strip("/").split("/")
+        branch = location[0]
+        if branch not in self.acl.get_branches():
+            self.perror(f"Invalid branch: {branch}")
+            return False
+        repo = location[1]
+        if repo not in self.acl.get_repositories():
+            self.perror(f"Invalid repository: {repo}")
+            return False
+        arch = location[2]
+        if arch not in self.acl.get_architectures():
+            self.perror(f"Invalid architecture: {arch}")
+            return False
+        packages = args.package
+        self.quiet = False
+        self.pfeedback(f"TODO - commit package to repo - using {packages}")
+        self.pfeedback(f"Reading files from workspace: {self.config.workspace}")
+        self.pfeedback(f"Commit endpoint is: {self.config.endpoint['pkgCommit']}")
+        self.pfeedback(f"Commit package(s) to: {branch}/{repo}/{arch}")
+        for pkg in packages:
+            self.pfeedback(f"commit package: {pkg}")
+        self.quiet = True
 
     def do_login(self, args):
         """
-        Login
-        :return: True/False
+        Retrieve JWT credentials for the configured bxt service
         """
         if not self.config.login():
-            print("Login failed!")
+            self.perror("Login failed!")
 
         self.prompt = f"({self.config.get_name()}@{self.config.get_hostname()}) $ "
 
     def do_configure(self, args):
         """
-        Reconfigure
-        :return: True/False
+        Configure endpoint and credentials
         """
         if not self.config.configure():
-            print("Configuration failed!")
+            self.perror("Configuration failed!")
 
 
 def start():
@@ -260,6 +379,9 @@ def start():
     Poetry entry point
     :return:
     """
+    if sys.platform.startswith("win"):
+        print(f"unsupported operating system: {sys.platform}")
+        exit(255)
     app = BxtCtl()
     sys.exit(app.cmdloop())
 
