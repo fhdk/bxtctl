32852790afbc42baa9f18baa818e818975ffcfb6
Frede Hundewadt <fhdk.6pmrw@8shield.net>
2024-07-17 09:16:26 +0200

    Add Utils.py with path_completion function
    A new file, Utils.py, has been added to the Bxt directory. This file includes the path_completion function, which generates a list of path completions for the bxt repository, using branches, repos, and archs as parameters.

---
+++ Bxt/Utils.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# bxtctl is command line client designed to interact with bxt api
+# bxt can be found at https://gitlab.com/anydistro/bxt
+#
+# bxtctl is free software: you can redistribute it and/or modify
+# it under the terms of the Affero GNU General Public License
+# as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# bxtctl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the Affero GNU General Public License
+# along with bxtctl.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Frede Hundewadt https://github.com/fhdk/bxtctl
+#
+from typing import List
+
+
+def path_completion(branches, repos, archs) -> List[str]:
+    """
+    generate a list of path completions for bxt repo
+    :param branches:
+    :param repos:
+    :param archs:
+    :return:
+    """
+    result = []
+    branches = list(branches())
+    repos = list(repos())
+    archs = list(archs())
+    for branch in branches:
+        for repo in repos:
+            for arch in archs:
+                result.append(f"{branch}/{repo}/{arch}")
+    return result
