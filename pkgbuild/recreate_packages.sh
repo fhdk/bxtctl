cec92e84d0e36b148af0931aedefaf38cdaceb36
Frede H <fh@uex.dk>
2024-09-05 16:51:49 +0200

    Update versioning process in package recreation script
    Introduced dynamic version generation using the current date and time in `recreate_packages.sh`. Updated the `PKGBUILD` and added version injection to maintain consistency across builds.

--- pkgbuild/recreate_packages.sh
+++ pkgbuild/recreate_packages.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 # git project root
 PROJECTDIR="$(git rev-parse --show-toplevel)"
+
 # test repos
 ARMDIR="$PROJECTDIR/repo/testing/extra/aarch64"
 X86DIR="$PROJECTDIR/repo/testing/extra/x86_64"
+
 # remove previous packages and signatures
 find "$PROJECTDIR/repo" -type f -name "a-dummy*" -exec rm "{}" \; > /dev/null
+
+# generate a version number
+pkgver=$(date -d "today" +"%Y%m%d.%H%M")
+
+# inject the version number into the PKGBUILD
+sed -i 's|pkgver=.*|pkgver='"${pkgver}"'|g' "$PROJECTDIR/pkgbuild/PKGBUILD"
+
+# and the upload scratchpad
+sed -i 's|pkgver = .*|pkgver = '\""${pkgver}"\"'|g' "$PROJECTDIR/scratchpads/0010_pkg_upload_using_bearer.py"
+
 # build new packages
 PKGDEST="$PROJECTDIR/repo" makepkg -cC
+
 # sign the new packages
 find "$PROJECTDIR/repo" -type f -name "*.pkg.tar.*" -exec signfile "{}" \;
+
 # loop the files
 for file in "$PROJECTDIR"/repo/*.pkg.tar.*
 do
@@ -19,6 +33,3 @@ do
   # clean up workspace
   rm "$file"
 done
-
-
-
