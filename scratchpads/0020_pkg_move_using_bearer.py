d419fcca7ed4d73e7f86674ba8047affc4d7402a
Frede H <fh@uex.dk>
2024-09-05 17:13:36 +0200

    Update User-Agent to use value from config
    Replaced hardcoded User-Agent string with a dynamic value sourced from the config file. This change enhances flexibility and maintainability by allowing the User-Agent to be easily modified without altering the code.

--- scratchpads/0020_pkg_move_using_bearer.py
+++ scratchpads/0020_pkg_move_using_bearer.py
@@ -68,7 +68,7 @@ files = {
 }
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0",
+    "User-Agent": config.user_agent,
     "Authorization": f"Bearer {token}",
     "x-bxtctl-token": str(uuid.uuid4())
 }
