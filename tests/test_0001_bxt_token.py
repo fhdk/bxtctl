465ea14daf6abe5f7f310455155699225946c93b
Frede H <fh@uex.dk>
2024-09-06 10:27:42 +0200

    Add setup method for BxtToken in test suite
    Initialized `BxtToken` with mock token and payload data in the `setUp` method of `Test0001BxtToken`. This ensures that each test within this test case has consistent and reusable token data for validation.

--- tests/test_0001_bxt_token.py
+++ tests/test_0001_bxt_token.py
@@ -7,7 +7,13 @@ from Bxt.BxtSession import BxtSession
 
 class Test0001BxtToken(unittest.TestCase):
     def setUp(self):
-        pass
+        self.bxt_token = BxtToken(token={
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE3MjU2MDA5MDAsImlhdCI6MTcyNTYwMDAwMCwiaXNzIjoiYnh0ZCIsImtpbmQiOiJhY2Nlc3NfdG9rZW4iLCJzdG9yYWdlIjoiYmVhcmVyIiwidXNlcm5hbWUiOiJieHR1c2VyIn0.wgar5KEEUR5j9ePPgCKaqHDwUedKtvKyZwGqUfwH3s4",
+            "refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE3MjY2MDAwMDAsImlhdCI6MTcyNTYwMDAwMCwiaXNzIjoiYnh0ZCIsImtpbmQiOiJyZWZyZXNoX3Rva2VuIiwic3RvcmFnZSI6ImJlYXJlciIsInVzZXJuYW1lIjoiYnh0dXNlciJ9.ZjzWG91Qxk64KTqp_QnvkX0oAX88ql9QvyFHFm-uSgQ",
+            "token_type": "bearer"
+        })
+        self.token_payload = {"exp": 1725600900, "iat": 1725600000, "iss": "bxtd", "kind": "access_token", "storage": "bearer", "username": "bxtuser" }
+        self.refresh_payload = {"exp": 1726600000, "iat": 1725600000, "iss": "bxtd", "kind": "refresh_token", "storage": "bearer", "username": "bxtuser"}
 
     def tearDown(self):
         pass
@@ -15,4 +21,3 @@ class Test0001BxtToken(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
