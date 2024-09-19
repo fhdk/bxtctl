77f4f2a00c764e7e124b8b420ec3a4f212a63e71
Frede H <fh@uex.dk>
2024-09-19 14:46:01 +0200

    Refactor token attribute names and add new tests
    Standardize token-related attribute names to improve readability and consistency. Introduce tests for access token retrieval, refresh token retrieval, and token owner validation to enhance test coverage.

--- tests/test_0001_bxt_token.py
+++ tests/test_0001_bxt_token.py
@@ -4,14 +4,13 @@ from bxtctl.Bxt.BxtToken import BxtToken
 
 class Test0001BxtToken(unittest.TestCase):
     def setUp(self):
-        self.bxt_token = BxtToken(
-            token={
-                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE3MjU2MDA5MDAsImlhdCI6MTcyNTYwMDAwMCwiaXNzIjoiYnh0ZCIsImtpbmQiOiJhY2Nlc3NfdG9rZW4iLCJzdG9yYWdlIjoiYmVhcmVyIiwidXNlcm5hbWUiOiJieHR1c2VyIn0.wgar5KEEUR5j9ePPgCKaqHDwUedKtvKyZwGqUfwH3s4",
-                "refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE3MjY2MDAwMDAsImlhdCI6MTcyNTYwMDAwMCwiaXNzIjoiYnh0ZCIsImtpbmQiOiJyZWZyZXNoX3Rva2VuIiwic3RvcmFnZSI6ImJlYXJlciIsInVzZXJuYW1lIjoiYnh0dXNlciJ9.ZjzWG91Qxk64KTqp_QnvkX0oAX88ql9QvyFHFm-uSgQ",
-                "token_type": "bearer",
-            }
-        )
-        self.token_payload = {
+        self.test_token = {
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE3MjU2MDA5MDAsImlhdCI6MTcyNTYwMDAwMCwiaXNzIjoiYnh0ZCIsImtpbmQiOiJhY2Nlc3NfdG9rZW4iLCJzdG9yYWdlIjoiYmVhcmVyIiwidXNlcm5hbWUiOiJieHR1c2VyIn0.wgar5KEEUR5j9ePPgCKaqHDwUedKtvKyZwGqUfwH3s4",
+            "refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE3MjY2MDAwMDAsImlhdCI6MTcyNTYwMDAwMCwiaXNzIjoiYnh0ZCIsImtpbmQiOiJyZWZyZXNoX3Rva2VuIiwic3RvcmFnZSI6ImJlYXJlciIsInVzZXJuYW1lIjoiYnh0dXNlciJ9.ZjzWG91Qxk64KTqp_QnvkX0oAX88ql9QvyFHFm-uSgQ",
+            "token_type": "bearer",
+        }
+
+        self.test_token_payload = {
             "exp": 1725600900,
             "iat": 1725600000,
             "iss": "bxtd",
@@ -19,7 +18,7 @@ class Test0001BxtToken(unittest.TestCase):
             "storage": "bearer",
             "username": "bxtuser",
         }
-        self.refresh_payload = {
+        self.test_refresh_payload = {
             "exp": 1726600000,
             "iat": 1725600000,
             "iss": "bxtd",
@@ -27,6 +26,30 @@ class Test0001BxtToken(unittest.TestCase):
             "storage": "bearer",
             "username": "bxtuser",
         }
+
+        self.token = BxtToken(self.test_token)
+
+    def test_get_access_token(self):
+        """
+        Test get access token
+        :return:
+        """
+        assert self.token.get_access_token() == self.test_token["access_token"]
+
+    def test_get_refresh_token(self):
+        """
+        Test get refresh token
+        :return:
+        """
+        assert self.token.get_access_token() == self.test_token["refresh_token"]
+
+    def test_token_owner(self):
+        """
+        Test if token is the expected owner
+        :return:
+        """
+        assert self.token.validate_owner("bxtuser") == True
+
 
     def tearDown(self):
         pass
