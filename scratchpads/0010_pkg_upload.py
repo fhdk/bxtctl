e488c8995cc8f522e4306a13f59076825c71eb32
Frede H <fh@uex.dk>
2024-09-30 10:08:27 +0200

    Update package version in PKGBUILD and script
    Bump package version from 20240915.1158 to 20240930.0950 in both the dummy-pkg/PKGBUILD file and scratchpads/0010_pkg_upload.py script. This ensures consistency in the package versioning across the build configuration and the upload process.

--- scratchpads/0010_pkg_upload.py
+++ scratchpads/0010_pkg_upload.py
@@ -63,7 +63,7 @@ to_section = {
 # workspace = f"{config.workspace}/testing/extra/aarch64"
 workspace = f"{config.get_workspace()}/testing/extra/x86_64"
 # dummies
-pkgver = "20240915.1158"
+pkgver = "20240930.0950"
 dummy1 = f"a-dummy1-{pkgver}-1-any.pkg.tar.zst"
 dummy2 = f"a-dummy2-{pkgver}-1-any.pkg.tar.zst"
 dummy3 = f"a-dummy3-{pkgver}-1-any.pkg.tar.zst"
